c0f51aff5bbe9ff0b89d75cf7e9f5d0295b5bfd9
Andre <fiebermodus@gmail.com>
2015-05-27 07:39:42 +0200

    Walking implemented
    made by Jendrik Bulk

--- StarbucksPrincess Proto/js/Game.js
+++ StarbucksPrincess Proto/js/Game.js
@@ -12,7 +12,8 @@ SBP.Game.prototype = {
  
   create: function() {
     var map;
-
+	var cursors;
+	this.game.physics.startSystem(Phaser.Physics.ARCADE); //Arcade Physics hinzufügen
     this.game.stage.backgroundColor = '#787878';
  	  // create map
  	  this.map = this.game.add.tilemap('level1');
@@ -37,20 +38,61 @@ SBP.Game.prototype = {
     //this.map.setCollisionBetween(1, 100000, true, 'blockedLayer');
     //create player
  
-    this.player = this.game.add.sprite(100, 300, 'player');
+    this.player = this.game.add.sprite(100, 700, 'player'); //Spieler erstellen, Startposition
     //physics on player
     this.game.physics.arcade.enable(this.player);
     //player gravity
-    this.player.body.gravity.y = 800;
+    this.player.body.gravity.y = 0;
     //Camera-Movement
     this.game.camera.follow(this.player);
-    
- 
+    this.player.body.collideWorldBounds = true; //Kollision des Spielers
+	
+	//this.player.animations.add('left', [0,1,2,3], 10, true); // Lauf-Animation
+	//this.player.animations.add('right', [5,6,7,8], 10, true);
+	this.cursors = this.game.input.keyboard.createCursorKeys(); //Pfeiltasten aktivieren
+	upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
+    downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
+    leftKey = this.game.input.keyboard.addKey(Phaser.Keyboard.LEFT);
+    rightKey = this.game.input.keyboard.addKey(Phaser.Keyboard.RIGHT);
  }, 
  
 
   update: function() {
-  	this.game.physics.arcade.collide(this.player, this.blockedLayer)
+  	this.game.physics.arcade.collide(this.player, this.blockedLayer) //Kollision mit Layer
+	 //  Reset the players velocity (movement)
+    this.player.body.velocity.x = 0; //sorgt dafür das nach Loslassen der Pfeiltasten die Spielfigur stehen bleibt
+	this.player.body.velocity.y = 0;
+
+    if (leftKey.isDown)
+    {
+        //  Move to the left
+        this.player.body.velocity.x =-150;
+        //this.player.animations.play('left');
+    }
+    else if (rightKey.isDown)
+    {
+        //  Move to the right
+        this.player.body.velocity.x = +150;
+        //this.player.animations.play('right');
+    }
+		//Bewegung nach oben
+	else if (upKey.isDown)
+	{
+		this.player.body.velocity.y = -150;
+		//this.player.animations.play('left');
+	}
+	else if (downKey.isDown)
+	{
+		//Bewegung nach unten
+		this.player.body.velocity.y = +150;
+		//this.player.animations.play('right');
+	}
+    else
+    {
+        //  Stand still
+        this.player.animations.stop();
+        //this.player.frame = 0;
+    }
   },
  
   render: function()
