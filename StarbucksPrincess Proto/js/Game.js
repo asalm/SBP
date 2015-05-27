886dfcd8b8d7e736829b436a798123103fa1a297
JendrikDerWeise <jbulk@stud.hs-bremen.de>
2015-05-27 13:42:50 +0200

    Animation

--- StarbucksPrincess Proto/js/Game.js
+++ StarbucksPrincess Proto/js/Game.js
@@ -7,13 +7,12 @@ SBP.Game.prototype = {
   preload: function() {
  
       this.game.time.advancedTiming = true;
- 
     },
  
   create: function() {
     var map;
 	var cursors;
-	this.game.physics.startSystem(Phaser.Physics.ARCADE); //Arcade Physics hinzufügen
+	//this.game.physics.startSystem(Phaser.Physics.ARCADE); //Arcade Physics hinzufügen
     this.game.stage.backgroundColor = '#787878';
  	  // create map
  	  this.map = this.game.add.tilemap('level1');
@@ -47,8 +46,8 @@ SBP.Game.prototype = {
     this.game.camera.follow(this.player);
     this.player.body.collideWorldBounds = true; //Kollision des Spielers
 	
-	//this.player.animations.add('left', [0,1,2,3], 10, true); // Lauf-Animation
-	//this.player.animations.add('right', [5,6,7,8], 10, true);
+	this.player.animations.add('left', [0,1,2,3], 10, true); // Lauf-Animation
+	this.player.animations.add('right', [5,6,7,8], 10, true);
 	this.cursors = this.game.input.keyboard.createCursorKeys(); //Pfeiltasten aktivieren
 	upKey = this.game.input.keyboard.addKey(Phaser.Keyboard.UP);
     downKey = this.game.input.keyboard.addKey(Phaser.Keyboard.DOWN);
@@ -67,13 +66,13 @@ SBP.Game.prototype = {
     {
         //  Move to the left
         this.player.body.velocity.x =-150;
-        //this.player.animations.play('left');
+        this.player.animations.play('left');
     }
     else if (rightKey.isDown)
     {
         //  Move to the right
         this.player.body.velocity.x = +150;
-        //this.player.animations.play('right');
+        this.player.animations.play('right');
     }
 		//Bewegung nach oben
 	else if (upKey.isDown)
