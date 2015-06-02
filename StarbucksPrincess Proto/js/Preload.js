478e4ad81d3066aa76627357516108010326ea70
Andre <fiebermodus@gmail.com>
2015-06-02 20:20:20 +0200

    neuer push, irgwas lief schief ..

--- StarbucksPrincess Proto/js/Preload.js
+++ StarbucksPrincess Proto/js/Preload.js
@@ -19,6 +19,15 @@ SBP.Preload.prototype = {
     this.load.image('Coffeebean', 'assets/images/bohne.png');
   //Mahlwerk sprite
     this.load.image('Mahlwerk', 'assets/images/Mahlwerk.png');
+
+      this.load.image('Mahlwerk', 'assets/images/Mahlwerk.png');
+   //Player sprite       //Player sprite
+ //Sounds
+ this.load.audio('walk', 'assets/sounds/walk.mp3');
+ this.load.audio('shoot', 'assets/sounds/shoot.mp3');
+ this.load.audio('death', 'assets/sounds/death.mp3');
+ this.load.audio('hit', 'assets/sounds/hit.mp3');
+ this.load.audio('jump', 'assets/sounds/jump.mp3');
   //Player sprite
     this.load.spritesheet('player', 'assets/char/nSlime_32x36_sheet.png', 32, 36);
 	this.load.spritesheet('dude', 'assets/dude.png', 32,48);
